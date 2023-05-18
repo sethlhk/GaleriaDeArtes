773ddb8e9151cdb60a189a19b47db4972a0874a2
Laura <lauramodesto465@gmail.com>
2023-05-18 17:04:42 -0300

    Initial commit

---
+++ app/src/main/java/com/example/galeriadeartes/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.galeriadeartes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.galeriadeartes.ui.theme.GaleriaDeArtesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GaleriaDeArtesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+@Preview
+@Composable
+fun galeria(){
+    Surface(
+        modifier = Modifier
+            .fillMaxSize(),
+        color = Color.Gray
+    ){
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(30.dp)
+    ) {
+     Artes (
+         painter = painterResource(id = R.drawable.lucifer)
+     )
+
+       }
+    }
+}
+@Composable
+fun Artes(painter: Painter){
+ Image(
+     painter = painter ,
+     contentDescription = null,
+     modifier = Modifier
+         .padding(top = 25.dp)
+         .fillMaxSize()
+ )
+}
